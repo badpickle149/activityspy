5b3a2ec2f8f63d49940bcc91b1e7abb4052261f9
Kevin Weng <kevinweng95@gmail.com>
2019-04-16 20:46:36 -0700

    initialized files

---
+++ app/src/main/java/edu/washington/weng2k17/activityspy/MainActivity.kt
@@ -0,0 +1,61 @@
+package edu.washington.weng2k17.activityspy
+
+import android.app.Application
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "Activity Spy"
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart event fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val text = "Greetings, UW!"
+        val duration = Toast.LENGTH_LONG
+
+        Toast.makeText(this, text, duration).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "We're going down, Captain!")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
